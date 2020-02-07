0273cb8ea695398eae0804b23fda62abad6b8a3a
Arrvindh Shriraman <ashriram@cs.sfu.ca>
2020-02-06 21:15:17 -0800

    Updated ALU and components list. Need to cross check

--- src/edu/sfu/cs295/Components.java
+++ src/edu/sfu/cs295/Components.java
@@ -8,30 +8,32 @@ import com.cburch.logisim.tools.Library;
 import com.cburch.logisim.tools.Tool;
 
 public class Components extends Library {
-    private List<Tool> tools;
+  private List<Tool> tools;
 
-    public Components() {
-        // Removed components from list 2/19/2019, will replace for P3
-        tools = Arrays.asList(new Tool[] {
-                new AddTool(new RegisterFile32()),
-                new AddTool(new Program32()),
-                new AddTool(new ALU()),
-                new AddTool(new Comparator()),
-                new AddTool(new Incrementer()),
-                new AddTool(new Video()),
-                new AddTool(new Ram()),
-                new AddTool(new SPIM()),
-                // new AddTool(new RamIO())
-        });
-    }
+  public Components() {
+    // Removed components from list 2/19/2019, will replace for P3
+    tools = Arrays.asList(new Tool[] {
+        new AddTool(new RegisterFile32()), new AddTool(new Program32()),
+        new AddTool(new ALU()),
+        new AddTool(new ALU2()), // Decode Black Box
+        new AddTool(new Bypass()), new AddTool(new Comparator()),
+        new AddTool(new Incrementer()), new AddTool(new Video()),
+        new AddTool(new Ram()), new AddTool(new SPIM()),
+        // new AddTool(new RamIO())
+    });
+  }
 
-    @Override
-    public String getName() { return "CS295-Components"; }
-    @Override
-    public String getDisplayName() { return "CS295 Components"; }
-    @Override
-    public List<Tool> getTools() { return tools; }
-    public boolean removeLibrary(String Name) {
-    	return false;
-    }
+  @Override
+  public String getName() {
+    return "CS295-Components";
+  }
+  @Override
+  public String getDisplayName() {
+    return "CS295 Components";
+  }
+  @Override
+  public List<Tool> getTools() {
+    return tools;
+  }
+  public boolean removeLibrary(String Name) { return false; }
 }
